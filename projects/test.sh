9cd94e2ab99542598ebae5967ac757b0dd1e0bc0
Miccah Castorina <m.castorina93@gmail.com>
2021-11-06 20:01:39 -0500

    Update test script to show expected results

--- projects/test.sh
+++ projects/test.sh
@@ -3,17 +3,45 @@
 # path to HardwareSimulator.sh
 hw_sim=/usr/bin/n2tHardwareSimulator
 
+_red='\e[0;31m'
+_grn='\e[0;32m'
+_rst='\e[39;49m'
+
+function compare_results {
+    local have_file="$1.out"
+    local want_file="$1.cmp"
+    local failure_line=$(wc --lines "$have_file" | cut -d' ' -f1)
+    echo -n '          '
+    head -n 1 "$want_file"
+    echo -e "expected:${_grn}" "$(sed -n ${failure_line}p "$want_file")$_rst"
+    echo -e "     got:${_red}" "$(tail -n 1 "$have_file")$_rst\n"
+}
+
 if [[ -z $1 ]]; then
     echo "please provide a directory containing *.tst files"
     exit 1
 fi
 
 cd "$1"
+total_count=0
+pass_count=0
+fail_names=""
 for file in *.tst; do
     printf "%-20s" "${file%.tst}"
     if $hw_sim $PWD/$file >/dev/null 2>&1; then
-        echo -e '\e[0;32mok\e[39;49m'
+        echo -e "${_grn}ok${_rst}"
+        pass_count=$((pass_count + 1))
     else
-        echo -e '\e[0;31mfail\e[39;49m'
+        echo -e "${_red}fail${_rst}"
+        compare_results "${file%.tst}"
+        fail_names="$fail_names, ${file%.tst}"
     fi
+    total_count=$((total_count + 1))
 done
+
+echo "========================================="
+if [[ $pass_count -ne $total_count ]]; then
+    echo "Failing modules: ${fail_names#, }" | fmt | sed -e '2,$s/^/                 /' | fmt
+    echo -n '        '
+fi
+echo "Summary: $pass_count/$total_count passing"
