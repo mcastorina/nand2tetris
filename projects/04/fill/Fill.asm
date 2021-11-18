d60154bb0e11656d58d58523e7c62ba6aa44d179
Miccah Castorina <m.castorina93@gmail.com>
2021-11-18 01:03:04 -0600

    Complete project 4/fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,54 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP1)
+// loop until KBD != 0
+@KBD
+D=M
+@LOOP1
+D;JEQ
+
+// blank screen
+@SCREEN
+D=A
+@save
+M=D
+(FILL_BLACK)
+
+@save    // load saved address
+A=M
+M=-1     // write 0xffff to address
+AD=A+1   // increment address
+@save    // save address
+M=D
+@KBD     // load end address
+D=A-D    // compare with current address
+@FILL_BLACK
+D;JGT    // keep looping if needed
+
+(LOOP2)
+@KBD
+D=M
+@LOOP2
+D;JNE
+
+// reset screen
+@SCREEN
+D=A
+@save
+M=D
+(FILL_WHITE)
+
+@save    // load saved address
+A=M
+M=0      // write 0x0000 to address
+AD=A+1   // increment address
+@save    // save address
+M=D
+@KBD     // load end address
+D=A-D    // compare with current address
+@FILL_WHITE
+D;JGT    // keep looping if needed
+
+@LOOP1
+0;JMP
